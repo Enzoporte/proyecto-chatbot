cf55af4397275a537c6ebae3c1eba2443fe56d28
PabloConstantinoUCU <PABLO.CONSTANTINO@correo.ucu.edu.uy>
2022-09-28 23:49:39 -0300

    Clases Pablo

---
+++ src/Library/AccesoBancoDeDatos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Chatbot
+{
+
+    public class AccesoBancoDeDatos
+    {
+        public string BancoDeDatos { get; set; }
+
+
+
+        public AccesoBancoDeDatos()
+        {
+
+        }
+
+
+        public string[,] TraerDatos(string BancoDeDatos)
+        {
+            
+        }
+
+
+    }
+}
